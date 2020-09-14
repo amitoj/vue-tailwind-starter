01a0b240081259b7d4d9211735a4c7d62d49b5a3
Amitoj Hanspal <ahanspal@pageone.co.uk>
2020-09-14 17:06:38 +0100

    Base master

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
-  <div id="app" class="antialiased text-gray-900 flex flex-col min-h-screen bg-gray-200">
-    <div class="flex-shrink-0 py-12 text-center">
-      <Hello></Hello>
-      <div class="mt-2">
-        <a href="https://github.com/james2doyle/vue-tailwind-starter" class="text-sm font-semibold text-gray-600 hover:underline">
-          Source on GitHub
-        </a>
-      </div>
-    </div>
+  <div id="app" class="bg-gray-100">
   </div>
 </template>
 
 <script>
-import Hello from 'Components/Hello.vue';
 
 export default {
   name: 'App',
   components: {
-    Hello,
   },
 };
 </script>
